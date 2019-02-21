98d5ef236b1bc3e43bd2e8e6bc9938212e46b099
charlie <lin891@purdue.edu>
2019-02-21 12:13:34 -0500

    Copy tool and length plotter changes for the length plotter to take in tsv files of steady-state lengths

--- length_plot/length_plotter.py
+++ length_plot/length_plotter.py
@@ -1,33 +1,77 @@
 import csv
+import sys
 import glob
 import matplotlib.pyplot as plt
 
 '''
-plots data
+Plot the steady-state length data
 '''
-fig = plt.figure()
-for fn in sorted(glob.glob("./data/*.csv")):
-    #read in data from file
-    with open(fn, 'rb') as csvfile:
-        #opens csv file and makes dict reader
-        reader = csv.DictReader(filter(lambda row: row[0] != '#', csvfile), skipinitialspace=True)
-        dataList = list(reader)#list of dictionaries
 
-    times = []
-    lengths = []
-    print dataList[0]
+def main():
+    folder = sys.argv[1]
+    textstr = "reduced volume = 0.65\n" + "viscosity ratio = 1.00"
+    leg_title = "alpha"
+    fn_out = "test"
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-#moving all the data to lists for plotting
-#spatial data in form of outer index = row num, inner index = step
-    for row in dataList:
-        times.append(float(row["time"]))
-        lengths.append(float(row["length"]))
+    for f_name in sorted(glob.glob("{}/*.tsv".format(folder))):
+        Ca_s_list, length_list, params = get_data(f_name)
+        ax.plot(Ca_s_list, length_list, 'o', label=params["alpha"])
+    
+    ax.set_title("Steady-state lengths")
+    ax.set_xlabel(r"Ca $\cdot \sqrt{\alpha}}$")
+    ax.set_ylabel(r"S-S length $(\frac{L}{a_0})$")
+    ax.legend(title=leg_title, bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0, prop={"size": 10})
 
-# plotting length vs time
-    plt.plot(times, lengths)
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
 
-plt.title('De/Ca comparison')
-plt.xlabel('time')
-plt.ylabel('Long axis length (L/a)')
-legend = plt.legend(["De = 1", "De = 10"], title="Reduced Volume")
-plt.savefig('length_plot.pdf', format='pdf', dpi=1000)
+    plt.tight_layout(rect=[0, 0, 0.85, 1])
+    plt.savefig("{}.pdf".format(fn_out), format="pdf")
+    plt.close()
+
+def get_data(filename):
+    """
+    Reads the data from the tsv file and returns a named tuple with the data values
+    """
+    # read header data:
+    with open(filename) as csv_file:
+        is_header = True
+        while is_header:
+            last_pos = csv_file.tell()
+            tmp_line = csv_file.readline()
+            if tmp_line.find('#') == 0:
+                eq_pos = tmp_line.find('=')
+                if tmp_line.find("alpha") != -1 and eq_pos != -1:
+                    alpha = float(tmp_line[eq_pos+1:])
+                elif tmp_line.find("viscRat") != -1 and eq_pos != -1:
+                    visc_rat = float(tmp_line[eq_pos+1:])
+                elif tmp_line.find("volRat") != -1 and eq_pos != -1:
+                    vol_rat = float(tmp_line[eq_pos+1:])
+                else:
+                    print("Unrecognized header line: {}".format(tmp_line))
+
+            else:
+                is_header = False
+                csv_file.seek(last_pos)
+
+        reader = csv.DictReader(csv_file, delimiter="\t")
+        data_list = list(reader)
+
+    # moving all the data to lists for plotting
+    params = {"alpha": alpha, "visc_rat": visc_rat, "vol_rat": vol_rat}
+    Ca_s_list = []
+    length_list = []
+    for row in data_list:
+        Ca_s_list.append(float(row["Ca_s"]))
+        length_list.append(float(row["SS_length"]))
+    try:
+        return (Ca_s_list, length_list, params)
+    except NameError:
+        print("missing a header variable")
+        return None
+
+
+if __name__ == "__main__":
+    main()
