d3defe5427d324eac408c614c91d44cb1cd59822
charlie <lin891@purdue.edu>
2019-05-02 14:15:12 -0400

    new effective strain rate plotter

--- crit_Ca_plot/visc_strain_plotter.py
+++ crit_Ca_plot/visc_strain_plotter.py
@@ -31,26 +31,19 @@ def main():
 
     folder = sys.argv[1]
 
-    unscaled_fig = plt.figure(figsize=(4.5, 4.5))
-    unscaled_ax = unscaled_fig.add_subplot(111)
-    scaled_fig = plt.figure(figsize=(4.5, 4.5))
-    scaled_ax = scaled_fig.add_subplot(111)
+    fig = plt.figure(figsize=(4.5, 4.5))
+    ax = fig.add_subplot(111)
 
     for filename in sorted(glob.glob("{}/*.csv".format(folder))):
-        unscaled_data, scaled_data = get_data(filename)
-        plot_crit_Ca(unscaled_ax, unscaled_data)
-        plot_crit_Ca(scaled_ax, scaled_data)
+        data = get_data(filename)
+        plot_crit_Ca(ax, data)
         style_cnt += 1
 
     textstr = r"$\nu=0.65$"
-    plot_final_setup(unscaled_fig, unscaled_ax, (0, 11), r"$Ca_{crit}$", textstr)
-    plot_final_setup(scaled_fig, scaled_ax, (0, 4), r"$Ca_{crit} \cdot \sqrt{\alpha}$", textstr)
+    plot_final_setup(fig, ax, (0, 4), r"$Ca_{crit} \cdot (\frac{1+\alpha}{2}) \cdot cos(2 \theta)$", textstr)
 
-    unscaled_fig.savefig(unscaled_data.file_name + ".pdf", format="pdf", dpi=1000)
-    plt.close(unscaled_fig)
-
-    scaled_fig.savefig(scaled_data.file_name + ".pdf", format="pdf", dpi=1000)
-    plt.close(scaled_fig)
+    fig.savefig(data.file_name + ".pdf", format="pdf", dpi=1000)
+    plt.close(fig)
 
 def get_data(filename):
     """
@@ -59,16 +52,12 @@ def get_data(filename):
     # read header data:
     with open(filename) as csv_file:
         is_header = True
-        scaled = False
         while is_header:
             last_pos = csv_file.tell()
             tmp_line = csv_file.readline()
             if tmp_line.find('#') == 0:
                 eq_pos = tmp_line.find('=')
-                if tmp_line.find("scaled") != -1 and eq_pos != -1:
-                    tmp1 = tmp_line[eq_pos+1:]
-                    scaled = str_to_bool(re.sub("\s+", '', tmp1))
-                elif tmp_line.find("viscRat") != -1 and eq_pos != -1:
+                if tmp_line.find("viscRat") != -1 and eq_pos != -1:
                     visc_rat = float(tmp_line[eq_pos+1:])
                 elif tmp_line.find("volRat") != -1 and eq_pos != -1:
                     vol_rat = float(tmp_line[eq_pos+1:])
@@ -82,10 +71,6 @@ def get_data(filename):
         # read critical Ca data
         reader = csv.DictReader(csv_file)
         data_list = list(reader)
-
-    if scaled:
-        print("Error: scaled data (use unscaled critcal Ca data)")
-        return
 
     # moving all the data to lists for plotting
     alpha = []
@@ -104,14 +89,10 @@ def get_data(filename):
         crit_Ca.append(np.mean((lower[i], upper[i])))
         error.append(abs(upper[i] - lower[i]) / 2.)
 
-    scaled_crit_Ca = [math.sqrt(alpha[i]) * crit_Ca[i] for i in range(num_data)]
-    scaled_error = [math.sqrt(alpha[i]) * error[i] for i in range(num_data)]
-
     Data_Tuple = collections.namedtuple("Data_Tuple", ["file_name", "y_label", "vol_rat", "visc_rat", "alpha", "Ca_data", "error"])
 
-    unscaled_data = Data_Tuple("unscaled_critical_Ca_plot", "critical Ca", vol_rat, visc_rat, alpha, crit_Ca, error)
-    scaled_data = Data_Tuple("scaled_critical_Ca_plot", "scaled critical Ca", vol_rat, visc_rat, alpha, scaled_crit_Ca, scaled_error)
-    return (unscaled_data, scaled_data)
+    data = Data_Tuple("strain_scaled_critical_Ca_plot", "critical Ca", vol_rat, visc_rat, alpha, crit_Ca, error)
+    return data
 
 def plot_crit_Ca(ax, plot_data):
     """
@@ -122,8 +103,8 @@ def plot_crit_Ca(ax, plot_data):
         x.set_marker('^')
     for y in caplines[1::2]:
         y.set_marker('v')
-    #for bar in barlinecols:
-        #bar.set_linestyle(LINE_STYLE_DICT[plot_data.visc_rat])
+    for bar in barlinecols:
+        bar.set_linestyle(LINE_STYLE_DICT[plot_data.visc_rat])
 
 def plot_final_setup(fig, ax, y_lim, y_label, textstr):
     # textbox
