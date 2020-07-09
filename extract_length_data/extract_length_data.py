7ab09ee87cb7ffd2b2c242d61ee6551b493ba481
charlie <charlielin7@gmail.com>
2020-07-09 16:17:16 -0400

    extract length in parallel

--- extract_length_data/extract_length_data.py
+++ extract_length_data/extract_length_data.py
@@ -4,52 +4,59 @@ extract the length data from a dictory full of .dat file
 write the extracted data into a csv file
 """
 
-import sys
 import glob
 import math
 import csv
 import os
+import argparse as argp
 import numpy as np
 import tec_dat as td
 
 def main():
-    in_dir = sys.argv[1]
-    in_dir = os.path.join(in_dir, '')
-    data, params = read_length_data(in_dir)
+    parser = argp.ArgumentParser(description="extracts length data from simulation data")
+    parser.add_argument("in_dir", help="folder of mesh data files")
+    parser.add_argument("mod", help="modulus for skipping files")
+    args = parser.parse_args()
+    data, params = read_length_data(args.in_dir, int(args.mod))
     write_length_data(data, params)
 
 
-def read_length_data(in_dir):
+def read_length_data(in_dir, mod):
     """
     reads in .dat files from
 
     Parameters:
         in_dir: directory of dat files
+        mod: modulus for skipping files
     Returns:
         length_data: list of maps that can be written out using
         csv.DictWriter
     """
     length_data = [] # list of maps to write
     for dat_file in sorted(glob.glob(in_dir + "*.dat")):
-        all_data, _f2v, params = td.read_dat(dat_file)
-        positions = []
-        for vert_data in all_data:
-            pos_data = [float(x) for x in vert_data[0:3]]
-            positions.append(pos_data)
-        time = params["time"]
-        Ca = params["strain_rate"] / params["EB"]
-        Ca_x = Ca * math.sin(2 * math.pi * params["W"] * params["time"])
-        x_len, y_len = td.calc_xy_lengths(np.array(positions))
-        major_len, minor_len = td.calc_major_minor_lengths(np.array(positions))
-        tmp_map = {
-            "time": time,
-            "Ca_x": Ca_x,
-            "x_len": x_len,
-            "y_len": y_len,
-            "major_len": major_len,
-            "minor_len": minor_len
-            }
-        length_data.append(tmp_map)
+        fn = os.path.basename(dat_file)
+        num = ''.join(i for i in fn if i.isdigit())
+        num = int(num)
+        if num % mod == 0:
+            all_data, _f2v, params = td.read_dat(dat_file)
+            positions = []
+            for vert_data in all_data:
+                pos_data = [float(x) for x in vert_data[0:3]]
+                positions.append(pos_data)
+            time = params["time"]
+            Ca = params["strain_rate"] / params["EB"]
+            Ca_x = Ca * math.sin(2 * math.pi * params["W"] * params["time"])
+            x_len, y_len = td.calc_xy_lengths(np.array(positions))
+            major_len, minor_len = td.calc_major_minor_lengths(np.array(positions))
+            tmp_map = {
+                "time": time,
+                "Ca_x": Ca_x,
+                "x_len": x_len,
+                "y_len": y_len,
+                "major_len": major_len,
+                "minor_len": minor_len
+                }
+            length_data.append(tmp_map)
     return (length_data, params)
 
 
@@ -82,11 +89,17 @@ def write_length_data(data, params):
         writer.writerows(data)
 
 
-def ext_run(in_dir):
+def ext_run(in_dir, mod):
     """
     For running through an external python program
+
+    Parameters:
+        in_dir: the input directory
+        mod: modulus for file skipping
+    Returns:
+        None
     """
-    data, params = read_length_data(in_dir)
+    data, params = read_length_data(in_dir, mod)
     write_length_data(data, params)
 
 
--- extract_length_data/parallel_extract_length.py
+++ extract_length_data/parallel_extract_length.py
@@ -3,22 +3,26 @@ Runs extract_length_data.py on all directories in folder
 Parallelized with Process by making each direcoty a separate process
 """
 from multiprocessing import Process
-import sys
 import os
 import glob
+import argparse as argp
 import extract_length_data as ELD
 
 def main():
-    parentFolder = sys.argv[1]
+    parser = argp.ArgumentParser(description="Runs several extral_length_data jobs in parallel")
+    parser.add_argument("in_dir", help="folder of tecdat files")
+    parser.add_argument("mod", help="modulus for skipping files")
+    args = parser.parse_args()
+    parentFolder = args.in_dir
     parentFolder = os.path.join(parentFolder, '')
     childFolders = glob.glob(parentFolder + "*/")
     processes = [
-        Process(target=ELD.ext_run, args=(childFolders[i],))
+        Process(target=ELD.ext_run, args=(childFolders[i], int(args.mod),))
         for i in range(len(childFolders))
     ]
 
-    for i in range(len(processes)):
-        processes[i].start()
+    for i, proc in enumerate(processes):
+        proc.start()
         print("Started {}".format(childFolders[i]))
 
     for p in processes:
